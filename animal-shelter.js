d03b3668387aabc06c0d8a6863695fc85ec44559
Joshua Lara <joshualara0910@gmail.com>
2024-02-13 15:11:53 -0700

    classes animals

---
+++ animal-shelter.js
@@ -0,0 +1,90 @@
+const animalData = require('./animal-data.json');
+
+
+
+class Animal {
+    constructor(name, species, color, hunger = 50) {
+        this.name = name
+        this.species = species
+        this.color = color
+        this.hunger = hunger
+    }
+    greet(){
+        console.log(`Hi, I'm ${this.name} the ${this.species}`)
+    }
+    feed(){
+        this.hunger -= 20;
+        console.log('Yum, I love food')
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, color, hunger = 50) {
+        super(name,'cat', color, hunger)
+        this.food = 'fish'
+    }
+    greet() {
+        console.log(`Meow, I'm ${this.name} the ${this.species}`)
+    }
+    feed() {
+        console.log(`Yum, I love ${this.food}`)
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, color, hunger) {
+        super(name, 'dog', color, hunger)
+        this.food = 'kibble'
+    }
+    greet() {
+        console.log(`Woof, I'm ${this.name} the ${this.species}`)
+    }
+    feed() {
+        console.log(`Yum, I love ${this.food}`)
+    }
+}
+
+
+class AnimalShelter {
+    constructor() {
+        this.animals = []
+    }
+    addAnimal(animal){
+        this.animals.push(animal)
+    }
+    adopt(animal) {
+        const animalIndex = this.animals.indexOf(animal);
+        this.animals.splice(animalIndex, 1);
+      }
+    getAnimalBySpecies(species) {
+        return this.animals.filter(a => a.species === species);
+    }
+}
+
+const shelter = new AnimalShelter()
+
+for (const a of animalData) {
+    const hunger = a.hunger ? a.hunger : 50;
+    if (a.species === 'dog') {
+        animal = new Dog(a.name, a.color, hunger)
+    } else if (a.species === 'cat') {
+        animal = new Cat(a.name, a.color, hunger)
+    } else {
+        const animal = new Animal(a.name, a.species, a.color, hunger);
+    }
+    shelter.addAnimal(animal);
+}
+
+for (const animal of shelter.animals) {
+    animal.greet();
+    animal.feed();
+  }
+
+let cat = new Cat()
+let dog = new Dog()
+
+console.log(cat)
+console.log(dog)
+
+dog.feed()
+
